7e626c25b78618c32a619067dd65f87159f8ce39
Acid3croco <yoann.ditte@gmail.com>
2020-03-19 13:53:36 +0100

    clean strat - add pckg lock to gitignore

--- src/algorithms/XmaRsiStrategy.ts
+++ src/algorithms/XmaRsiStrategy.ts
@@ -1,5 +1,5 @@
 import Strategy, {StrategyParams} from "./Strategy";
-import { EntryType, reverseIndex, rsi, sma, Trade, TradeTypes, Zlema } from "@jordanbonaldi/indicatorsapi";
+import { EntryType, reverseIndex, rsi, sma, Trade, TradeTypes, Alma } from "@jordanbonaldi/indicatorsapi";
 import { CandleChartResult } from "@jordanbonaldi/binancefetcher";
 
 export interface XmaRsiInput extends StrategyParams {
@@ -40,26 +40,6 @@ export default new class XmaRsiStrategy extends Strategy<XmaRsiInput> {
             console.log("RSI: " + myRsi.slice(myRsi.length - 4, 999));
             console.log("xMA RSI: " + myXmaRsi.slice(myXmaRsi.length - 4, 999));
         };
-
-        let Alma: Function = (input: { period: number, values: number[], offset: number, sigma: number }): number[] => {
-            let almaValues: number[] = [];
-
-            let almaCandle: Function = (candleIndex: number): number => {
-                let m: number = Math.floor(input.offset * (input.period - 1));
-                let s: number = input.period / input.sigma;
-                let weight: number = 0, norm: number = 0, sum: number = 0;
-                for (let a = 0; a < input.period; a++) {
-                    weight = Math.exp(-1 * Math.pow(a - m, 2) / (2 * Math.pow(s, 2)))
-                    norm = norm + weight
-                    sum = sum + (reverseIndex(input.values, candleIndex + input.period - 1 - a) * weight)
-                }
-                return (sum / norm)
-            }
-
-            for (let candleIndex: number = input.values.length - input.period; candleIndex >= 0; candleIndex--)
-                almaValues.push(almaCandle(candleIndex));
-            return almaValues.reverse();
-        }
 
         let xmaCandles = candles.map(c => parseFloat(c.close)).slice(0, -1);
         let rsiCandles = candles.map(c => parseFloat(c.close)).slice(0, -1);
