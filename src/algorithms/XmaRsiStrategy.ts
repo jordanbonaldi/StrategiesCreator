8428a8d735e49219d47a1f6143a59ea4685e2ffd
Acid3croco <yoann.ditte@gmail.com>
2020-03-19 01:02:01 +0100

    add alma

--- src/algorithms/XmaRsiStrategy.ts
+++ src/algorithms/XmaRsiStrategy.ts
@@ -1,6 +1,6 @@
 import Strategy from "./Strategy";
-import {EntryType, reverseIndex, rsi, sma, Trade, TradeTypes, Zlema} from "@jordanbonaldi/indicatorsapi";
-import {CandleChartResult} from "@jordanbonaldi/binancefetcher";
+import { EntryType, reverseIndex, rsi, sma, Trade, TradeTypes, Zlema } from "@jordanbonaldi/indicatorsapi";
+import { CandleChartResult } from "@jordanbonaldi/binancefetcher";
 
 export default new class XmaRsiStrategy extends Strategy {
 
@@ -22,28 +22,49 @@ export default new class XmaRsiStrategy extends Strategy {
     algorithm(candles: CandleChartResult[], assetDetail: string, assetTimeFrame: string, params: any, trade: Trade | undefined): Trade {
         if (!params.hasOwnProperty('xmaPeriod'))
             params = {
-                xmaPeriod: 350,
-                xmaRsiPeriod: 350,
-                rsiPeriod: 250,
+                xmaPeriod: 21,
+                xmaRsiPeriod: 21,
+                rsiPeriod: 14,
                 stopLoss: 3.5
             };
 
         let printDebug: Function = (): void => {
             console.log(`Candles length: ${candles.length}`);
-            console.log("xmaCandles: " + xmaCandles[xmaCandles.length - 1]);
             console.log("candles: " + candles.map(c => c.close)[998]);
-            console.log("xMA: " + myXma.slice(myXma.length - 4, 999));
+            console.log("xMA: " + myXma);
+            console.log("xMA LEN: " + myXma.length);
             console.log("RSI: " + myRsi.slice(myRsi.length - 4, 999));
             console.log("xMA RSI: " + myXmaRsi.slice(myXmaRsi.length - 4, 999));
         };
+
+        let Alma: Function = (input: { period: number, values: number[], offset: number, sigma: number }): number[] => {
+            let almaValues: number[] = [];
+
+            let almaCandle: Function = (candleIndex: number): number => {
+                let m: number = Math.floor(input.offset * (input.period - 1));
+                let s: number = input.period / input.sigma;
+                let weight: number = 0, norm: number = 0, sum: number = 0;
+                for (let a = 0; a < input.period; a++) {
+                    weight = Math.exp(-1 * Math.pow(a - m, 2) / (2 * Math.pow(s, 2)))
+                    norm = norm + weight
+                    sum = sum + (reverseIndex(input.values, candleIndex + input.period - 1 - a) * weight)
+                }
+                return (sum / norm)
+            }
+
+            for (let candleIndex: number = input.values.length - input.period; candleIndex >= 0; candleIndex--)
+                almaValues.push(almaCandle(candleIndex));
+            return almaValues.reverse();
+        }
 
         console.log(params);
 
         let xmaCandles = candles.map(c => parseFloat(c.close)).slice(0, -1);
         let rsiCandles = candles.map(c => parseFloat(c.close)).slice(0, -1);
-        let myXma = Zlema({ period: params.xmaPeriod, values: xmaCandles });
+        let myXma: number[] = Alma({ period: params.xmaPeriod, values: xmaCandles, offset: 0.5, sigma: 6 });
         let myRsi = rsi({ period: params.rsiPeriod, values: rsiCandles });
         let myXmaRsi = sma({ period: params.xmaRsiPeriod, values: myRsi });
+        //printDebug()
 
         let isXmaBull = reverseIndex(myXma) > reverseIndex(myXma, 1);
         let isXmaRsiBull = reverseIndex(myXmaRsi) > reverseIndex(myXmaRsi, 1);
