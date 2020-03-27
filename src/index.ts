061529cf46f53b84723a79dc2aee0ee4aa5e593d
Neferett <jordan.bonaldi@epitech.eu>
2020-03-27 18:47:34 +0100

    Index updatedé

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Config from "../config/Config";
 import Strategy from "./strategies/Strategy";
 import StrategyHandler from "./handlers/StrategyHandler";
+import StrategyResult from "./entity/StrategyResult";
+import Trade from "./entity/Trade"
+import {TradeTypes, EntryType} from "./entity/TradeTypes"
 
 let strategies: Strategy<any>[] = Config.strategies;
 
@@ -9,4 +12,8 @@ export default StrategyHandler;
 export {
     strategies,
     Strategy as Strategy,
+    StrategyResult as StrategyResult,
+    Trade as Trade,
+    TradeTypes as TradeTypes,
+    EntryType as EntryType
 }
