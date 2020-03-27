2598317935b4c4e8ce8845d5fff6f963423efc97
Acid3croco <yoann.ditte@gmail.com>
2020-03-27 19:45:58 +0100

    XmaRsi strat update trade system

--- src/strategies/XmaRsiStrategy.ts
+++ src/strategies/XmaRsiStrategy.ts
@@ -1,23 +1,26 @@
-import Strategy, {StrategyParams} from "./Strategy";
-import {Alma, reverseIndex, rsi, sma} from "@jordanbonaldi/indicatorsapi";
-import {CandleModel} from "@jordanbonaldi/binancefetcher";
-import {RiskType} from "../entity/BacktestParams";
+import Strategy, { StrategyParams } from "./Strategy";
+import { Alma, reverseIndex, rsi, sma } from "@jordanbonaldi/indicatorsapi";
+import { CandleModel } from "@jordanbonaldi/binancefetcher";
+import { RiskType } from "../entity/BacktestParams";
 import Trade from "../entity/Trade";
-import {EntryType, TradeTypes} from "../entity/TradeTypes";
+import { EntryType, TradeTypes } from "../entity/TradeTypes";
 
 export class XmaRsiInput implements StrategyParams {
     asset = 'BTCUSDT';
-    timeframe =  '1h';
+    timeframe = '1h';
     xmaPeriod = 21;
     xmaRsiPeriod = 21;
     rsiPeriod = 14;
-    stopLoss =  3.5;
+    exit = {
+        useStopLoss: true,
+        stopPerc: 3.5
+    };
 }
 
 export default new class XmaRsiStrategy extends Strategy<XmaRsiInput> {
     constructor() {
         super('XmaRsiStrategy', new XmaRsiInput(), {
-            equity: 0, riskInTrade: 0, riskType: RiskType.FIXED_AMOUNT, warm_up: 0
+            equity: 100, riskInTrade: 90, riskType: RiskType.FIXED_AMOUNT, warm_up: 70
         });
     }
 
@@ -38,42 +41,46 @@ export default new class XmaRsiStrategy extends Strategy<XmaRsiInput> {
             console.log("RSI: " + myRsi.slice(myRsi.length - 4, 999));
             console.log("xMA RSI: " + myXmaRsi.slice(myXmaRsi.length - 4, 999));
         };
+        let lastCandle: CandleModel = reverseIndex(candles, 1);
+
         let xmaCandles = candles.map(c => c.close).slice(0, -1);
         let rsiCandles = candles.map(c => c.close).slice(0, -1);
         let myXma: number[] = Alma({ period: params.xmaPeriod, values: xmaCandles, offset: 0.5, sigma: 6 });
         let myRsi = rsi({ period: params.rsiPeriod, values: rsiCandles });
         let myXmaRsi = sma({ period: params.xmaRsiPeriod, values: myRsi });
-        printDebug();
+        //printDebug();
 
         let isXmaBull = reverseIndex(myXma) > reverseIndex(myXma, 1);
         let isXmaRsiBull = reverseIndex(myXmaRsi) > reverseIndex(myXmaRsi, 1);
 
-        console.log(isXmaBull)
-        console.log(isXmaRsiBull)
+        let longCond = isXmaBull && isXmaRsiBull
+        let stoploss: number = params.exit.useStopLoss ? lastCandle.close * (1 - params.exit.stopPerc / 100) : 0;
 
-        return isXmaBull && isXmaRsiBull ?
-            {
+        let currentTrade: Trade | undefined =
+            longCond ? {
                 entryType: EntryType.ENTRY,
                 type: TradeTypes.LONG,
-                price: reverseIndex(candles, 1).close,
-                stoploss: reverseIndex(candles, 1).close * (1 - params.stopLoss / 100),
+                price: lastCandle.close,
+                stoploss: trade && trade.stoploss > 0 ? trade.stoploss : stoploss,
                 asset: this.defaultParams.asset,
                 timeframe: this.defaultParams.timeframe,
-            } : !isXmaBull && !isXmaRsiBull ?
-                {
-                    entryType: EntryType.ENTRY,
-                    type: TradeTypes.SHORT,
-                    price: reverseIndex(candles, 1).close,
-                    stoploss: reverseIndex(candles, 1).close * (1 + params.stopLoss / 100),
-                    asset: this.defaultParams.asset,
-                    timeframe: this.defaultParams.timeframe,
-                } : {
-                    entryType: EntryType.NOTHING,
-                    type: TradeTypes.LONG,
-                    price: 0,
-                    stoploss: 0,
-                    asset: this.defaultParams.asset,
-                    timeframe: this.defaultParams.timeframe,
-                };
+            } : (!longCond && trade || (trade && lastCandle.close < trade.stoploss)) && trade?.entryType == EntryType.ENTRY ? {
+                entryType: EntryType.EXIT,
+                type: TradeTypes.LONG,
+                price: lastCandle.close < trade.stoploss ? trade.stoploss : lastCandle.close,
+                stoploss: 0,
+                asset: this.defaultParams.asset,
+                timeframe: this.defaultParams.timeframe,
+            } : undefined;
+
+        return currentTrade ? currentTrade : {
+            entryType: EntryType.NOTHING,
+            type: TradeTypes.LONG,
+            price: 0,
+            stoploss: 0,
+            asset: this.defaultParams.asset,
+            timeframe: this.defaultParams.timeframe,
+
+        }
     }
 }
--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,7 @@ import BinanceAlgorithmTests from "./tests/BinanceStrategiesTests";
 import StrategyResult from "./entity/StrategyResult";
 
 BinanceAlgorithmTests.connect()
-    .then(instance => instance.runBacktestOnAsset('IchimokuLongStrategy', {
+    .then(instance => instance.runBacktestOnAsset('XmaRsiStrategy', {
         asset: 'BTCUSDT',
         timeframe: '1d'
     }))
